16a9cae40657b802ae2a673815b5af266a460043
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-15 03:02:50 -0500

    chore: add read access

--- terraform/sso-groups.tf
+++ terraform/sso-groups.tf
@@ -33,9 +33,38 @@ resource "aws_ssoadmin_account_assignment" "full_admin" {
 }
 
 # ==
-# Human Group
+# Human Group - full read
 # ==
+resource "aws_identitystore_group" "human_full_read" {
+  identity_store_id = local.sso_identity_store_id
+  display_name      = "full-read"
+  description       = "human - full read privilege group"
+}
 
+resource "aws_ssoadmin_permission_set" "human_full_read" {
+  instance_arn     = local.sso_instance_arn
+  name             = "full-read"
+  description      = "human - full read privileges"
+  session_duration = "PT2H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "human_full_read" {
+  instance_arn       = local.sso_instance_arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.human_full_read.arn
+  depends_on         = [aws_ssoadmin_account_assignment.human_full_read]
+}
+
+resource "aws_ssoadmin_account_assignment" "human_full_read" {
+  count = length(local.all_account_ids)
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.human_full_read.arn
+  principal_type     = "GROUP"
+  principal_id       = aws_identitystore_group.human_full_read.group_id
+  target_type        = "AWS_ACCOUNT"
+  target_id          = local.all_account_ids[count.index]
+}
 
 # ==
 # Infra (deploy) Group
